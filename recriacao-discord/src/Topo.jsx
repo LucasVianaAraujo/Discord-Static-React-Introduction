775f86020636be3ee03cbacf445260152fb182eb
Lucas Viana <luks79180@gmail.com>
2025-07-23 20:14:52 -0300

    Version -- 1.1

---
+++ recriacao-discord/src/Topo.jsx
@@ -0,0 +1,17 @@
+function Top() {
+    return (
+        <div className="top">
+            <img src='src/assets/discord-logo.png' width="160px" height="100px" alt="discord logo" />
+            <div className="links">
+                <div className="borda">
+                    <a href="#">Feedback</a>
+                    <a href="#">English (US) ↓</a>
+                    <a href="#">Submit a request</a>
+                    <a href="#">Sign in</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Top;
